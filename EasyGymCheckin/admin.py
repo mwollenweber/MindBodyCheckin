6f3cd64fe109aae85027ee08f82be69bf4873509
Matthew Wollenweber <mjw@insomniac.tech>
2024-02-24 20:27:55 -0600

    wip

--- EasyGymCheckin/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import gymClass, gymClient, gymCheckin
-
-admin.site.register(gymClass)
-admin.site.register(gymClient)
-admin.site.register(gymCheckin)
